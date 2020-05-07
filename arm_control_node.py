07bcf7a7890a40d773fe2f0db9c39d58e37fd09c
MikeFischer1 <fischems@lafayette.edu>
2020-05-07 14:24:17 +0000

    Reverted control node

--- arm_control_node.py
+++ arm_control_node.py
@@ -37,12 +37,13 @@ class arm_controller():
 		self.joint2_angle = rospy.Publisher('/openArm/joint2_position_controller/command', Float64, queue_size=1)
 		self.joint3_angle = rospy.Publisher('/openArm/joint3_position_controller/command', Float64, queue_size=1)
 		self.joint4_angle = rospy.Publisher('/openArm/joint4_position_controller/command', Float64, queue_size=1)
+		self.old_arm_action = 'rest'
 		self.arm_action = 'rest'
 		self.button_xyz = [0.0,0.0,0.0]
+		self.button_data = [0.0,0.0,0.0]
 
 		# create loop
 		rospy.Timer(rospy.Duration(self.dT), self.loop, oneshot=False)
-		self.arm_action = 'rest'
 
 
 	def loop(self, event):
@@ -57,6 +58,9 @@ class arm_controller():
 		self.time_elapsed = self.timenow-self.starttime
 
 		# find angles based on IK coordinate frame
+		if self.old_arm_action != self.arm_action:
+			self.button_xyz = self.button_data
+
 		IK_angles = self.IK.joint_angles(self.arm_action, self.button_xyz, self.time_elapsed)
 
 		# convert angles to arm controller coordinate frame
@@ -70,13 +74,14 @@ class arm_controller():
 
 
 	def actioncallback(self,data):
-
+		
+		self.old_arm_action = self.arm_action
 		self.arm_action = data.data
 		self.starttime = time.time()
 
 	def buttoncallback(self,data):
 
-		self.button_xyz = data.data
+		self.button_data = data.data
 
 
 # main function
